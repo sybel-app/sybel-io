65241aba696907b1f110defdcc29d5819c19435e
Quentin <quentin@nivelais.com>
2022-06-09 09:56:47 +0200

    [SmartContract] Start to integrate smart contract into the porject

---
+++ hardhat.config.js
@@ -0,0 +1,12 @@
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: "0.8.4",
+  settings: {
+    optimizer: {
+      enabled: true,
+      runs: 1000,
+    },
+  },
+};
