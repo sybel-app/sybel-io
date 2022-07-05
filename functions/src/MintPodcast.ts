4b07ac3a4625ebb3d25c2363ce128383fc7b955e
Quentin <quentin@nivelais.com>
2022-07-05 10:20:40 +0200

    [SybelBack] Review minting implementation to be a bit  less resources angry

--- functions/src/MintPodcast.ts
+++ functions/src/MintPodcast.ts
@@ -2,9 +2,11 @@ import * as functions from "firebase-functions";
 import cors from "cors";
 import { buildFractionId, allTokenTypesToRarity } from "./utils/SybelMath";
 import { Storage } from "@google-cloud/storage";
-import { getMinterConnected } from "./utils/Contract";
+import { Event } from "ethers";
 import { NftMetadata } from "./utils/NftMetadata";
 import { getWalletForUser } from "./utils/UserUtils";
+import { PodcastMintedEvent } from "./generated-types/Minter";
+import { minterConnected } from "./utils/Contract";
 
 /**
  * @function
@@ -43,27 +45,44 @@ export default () =>
 
         try {
           // Get our minter contract, connected via the sybel wallet
-          const minterConnected = getMinterConnected();
-          const podcastMintedFilter = minterConnected.filters.PodcastMinted();
+          const minter = await minterConnected();
           // Try to mint a new podcast
-          await minterConnected.addPodcast(
+          const mintPodcastTx = await minter.addPodcast(
             requestDto.supply[0],
             requestDto.supply[1],
             requestDto.supply[2],
             requestDto.supply[3],
             creatorWallet.address
           );
-          const filteredEvents = await minterConnected.queryFilter(
-            podcastMintedFilter
+          // Await that the transaction pass on the blockchain
+          const mintPodcastTxReceipt = await mintPodcastTx.wait();
+          const mintPodcastEvents = mintPodcastTxReceipt.events?.filter(
+            (event: Event): event is PodcastMintedEvent =>
+              event.args?.baseId != null
+          );
+          // If we are unable to extract the mint event, exit directly
+          if (!mintPodcastEvents) {
+            response.status(500).send({
+              error: `Unable to find the mind podcast event, you should check it manually, tx hash ${mintPodcastTxReceipt.blockHash}`,
+            });
+            return;
+          }
+          // Otherwise, extract the mint event, and use it to generate metadata
+          const mintPodcastEvent = mintPodcastEvents[0];
+          functions.logger.debug(
+            `Found the mint podcast event from the tx ${mintPodcastTxReceipt.blockHash}, with podcast id ${mintPodcastEvent.args.baseId}  `
           );
           // Generate all of required JSON
-          await Promise.all(
+          const uploadedFiles = await Promise.all(
             allTokenTypesToRarity.map(async (tokenTypeToRarity) => {
               // Find the id of the generated fraction
               const fractionId = buildFractionId(
-                filteredEvents[filteredEvents.length - 1].args[3],
+                mintPodcastEvent.args.baseId,
                 tokenTypeToRarity.tokenTypes
               ).toNumber();
+              functions.logger.debug(
+                `Build the fraction id ${fractionId} from base id ${mintPodcastEvent.args.baseId} and token type ${tokenTypeToRarity.tokenTypes}`
+              );
               // Build the json metadata we will upload
               const nftMetadata = new NftMetadata(
                 fractionId,
@@ -74,17 +93,17 @@ export default () =>
                 tokenTypeToRarity.rarity
               );
               // Then upload the built metadata
-              await uploadFile(nftMetadata.toJson(), fractionId.toString());
-            })
-          ).then(() =>
-            // Build our response
-            response.status(200).json({
-              transactionHash:
-                filteredEvents[filteredEvents.length - 1].transactionHash,
-              from: filteredEvents[filteredEvents.length - 1].args[1],
-              to: filteredEvents[filteredEvents.length - 1].args[2],
+              return await uploadFile(nftMetadata.toJson(), fractionId);
             })
           );
+          // Then send our response
+          response.status(200).json({
+            transactionHash: mintPodcastEvent.transactionHash,
+            podcastId: mintPodcastEvent.args.baseId,
+            owner: mintPodcastEvent.args.owner,
+            operator: mintPodcastTxReceipt.from,
+            metadataGenerated: uploadedFiles,
+          });
         } catch (error) {
           functions.logger.debug(
             "An error occured while minting the podcast",
@@ -95,10 +114,21 @@ export default () =>
       });
     });
 
-// Define the function that will upload a file on our bucket
-async function uploadFile(json: string, filename: string) {
+/**
+ * Upload a given json into our bucket
+ * @param {string} json
+ * @param {string} fractionId
+ * @return {void}
+ */
+async function uploadFile(json: string, fractionId: number): Promise<string> {
   const storage = new Storage();
-  const bucket = storage.bucket("gs://sybel-io.appspot.com");
-  const file = bucket.file("json/" + filename + ".json");
-  return await file.save(json);
+  // Upload our file
+  const file = storage
+    .bucket("gs://sybel-io.appspot.com")
+    .file(`json/${fractionId}.json`);
+  // Upload the json and make it public
+  await file.save(json);
+  await file.makePublic();
+  // Return the public url of this file
+  return file.publicUrl();
 }
